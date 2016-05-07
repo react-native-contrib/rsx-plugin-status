993456a2cff409c369aec254ab3a4b1e226367e0
Hassan Khan <contact@hassankhan.me>
2016-05-07 14:37:23 +0100

    Cleaned up code

--- src/status.js
+++ src/status.js
@@ -1,25 +1,21 @@
 const utils = require('rsx-common');
-const log = utils.log;
 const path = require('path');
 
-const appRoot = process.env['RN_PROJECT_ROOT'];
+const log = utils.log;
 
-const getInstalledDependencyVersion = (dependency) => {
-  return utils.getProjectPackageJson(
+const appRoot = process.env.RN_PROJECT_ROOT;
+const dependencies = ['react', 'react-native'];
+
+const getInstalledVersion = (dependency) => {
+  return utils.project.getPackageJson(
     path.join(appRoot, 'node_modules', dependency)
   ).version;
-}
-
-const logDependency = (dependency) => {
-  log.info(dependency + ': ' + getInstalledDependencyVersion(dependency));
-}
+};
 
 module.exports = function status(args, callback) {
   log.heading = 'rsx-status';
 
-  const pjson = utils.getProjectPackageJson(appRoot);
-  [
-    'react',
-    'react-native',
-  ].forEach(logDependency);
+  dependencies.forEach((dependency) => {
+    log.info(dependency + ': ' + getInstalledVersion(dependency));
+  });
 };
